95988f9842edf46cef0bdecafa3fe9256de396d6
edy <edycoleee@gmail.com>
2025-11-30 07:29:13 +0700

    gitignore

---
+++ mnist-dasar/backend/app.py
@@ -0,0 +1,39 @@
+# backend/app.py
+from flask import Flask, request, jsonify
+from tensorflow.keras.models import load_model
+import numpy as np
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+# Load model CIFAR
+model = load_model("model/cifar_model.h5")
+class_names = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
+
+def preprocess_image(image_bytes):
+    img = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    img = img.resize((32, 32))  # CIFAR ukuran 32x32
+    img_array = np.array(img) / 255.0
+    return np.expand_dims(img_array, axis=0)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if "file" not in request.files:
+        return jsonify({"error": "No file uploaded"}), 400
+    
+    file = request.files["file"]
+    img_bytes = file.read()
+    processed = preprocess_image(img_bytes)
+    
+    preds = model.predict(processed)
+    class_id = np.argmax(preds[0])
+    confidence = float(np.max(preds[0]))
+    
+    return jsonify({
+        "class": class_names[class_id],
+        "confidence": confidence
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
